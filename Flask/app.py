b2a5c0bdd160701a0aa1a64998686ee35737b8a6
Gabriel Palma <144812765+GabrielP012@users.noreply.github.com>
2024-11-04 21:37:12 -0500

    Add files via upload

--- Flask/app.py
+++ Flask/app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+
 
 app = Flask(__name__)
 
@@ -14,5 +15,5 @@ def about():
 def contact():
     return render_template('contact.html')
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(debug=True)
